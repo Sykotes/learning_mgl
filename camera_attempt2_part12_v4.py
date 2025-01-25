373d9ddf6e9cb2ccc1eadb1dd809031679827e0e
Sykotes <sykotes@icloud.com>
2025-01-25 23:06:11 +0000

    camera moves ish

--- camera_attempt2_part12_v4.py
+++ camera_attempt2_part12_v4.py
@@ -6,28 +6,70 @@ import pygame
 
 class Camera:
     def __init__(self) -> None:
-        self.position = glm.vec3(2, 0, 2)
-        self.pitch = glm.radians(-90)
-        self.yaw = 0
+        self.position = glm.vec3(2, 0, 2)  # Initial position of the camera
+        direction = glm.vec3(0, 0, 0) - self.position  # Vector to the origin
+
+        # Calculate pitch and yaw to look at the origin
+        self.pitch = glm.asin(direction.y / glm.length(direction))  # Pitch angle
+        self.yaw = glm.atan(direction.z, direction.x)  # Yaw angle
+
+        print(glm.degrees(self.pitch), glm.degrees(self.yaw))
+
         self.up = glm.vec3(0, 1, 0)
         self.forward = glm.vec3(0, 0, -1)
-        self.right = glm.vec3(1, 0, 0)
 
-        self.view = glm.lookAt(self.position, glm.vec3(0), self.up)
+        self.update_forward()
+        self.view = glm.lookAt(self.position, self.position + self.forward, self.up)
         self.proj = glm.perspective(glm.radians(60), 4 / 3, 0.1, 100)
         self.model = glm.mat4()
+
+    def update_forward(self) -> None:
+        self.forward.x = glm.cos(self.pitch) * glm.cos(self.yaw)
+        self.forward.y = glm.sin(self.pitch)
+        self.forward.z = glm.cos(self.pitch) * glm.sin(self.yaw)
+        self.forward = glm.normalize(self.forward)
+
+    def update(self) -> None:
+        self.move()
+        self.update_forward()
+        self.view = glm.lookAt(self.position, self.position + self.forward, self.up)
+
+    def move(self) -> None:
+        self.position += self.forward * 0.01
+
+
+# class Camera:
+#     def __init__(self) -> None:
+#         self.position = glm.vec3(2, 0, 2)
+#         self.pitch = glm.radians(-90)
+#         self.yaw = 0
+#         self.up = glm.vec3(0, 1, 0)
+#         self.forward = glm.vec3(0, 0, -1)
+#
+#         self.view = glm.lookAt(self.position, glm.vec3(0), self.up)
+#         self.proj = glm.perspective(glm.radians(60), 4 / 3, 0.1, 100)
+#         self.model = glm.mat4()
+#
+#     def update(self) -> None:
+#         self.move()
+#         self.view = glm.lookAt(self.position, glm.vec3(0), self.up)
+#
+#     def move(self) -> None:
+#         self.position.xz += 0.001
 
 
 class Game:
     def __init__(self) -> None:
-        pygame.init()
+        _ = pygame.init()
         _ = pygame.display.set_mode((800, 600), pygame.OPENGL | pygame.DOUBLEBUF)
 
         self.ctx = mgl.create_context()
 
         self.camera = Camera()
-
         self.setup_render()
+
+        self.clock = pygame.Clock()
+        self.running = True
 
     def setup_render(self) -> None:
         self.program = self.ctx.program(
@@ -77,21 +119,23 @@ class Game:
     def render(self) -> None:
         self.ctx.clear(0.17, 0.2, 0.3)
 
-        self.program["model"].write(self.camera.model)
-        self.program["proj"].write(self.camera.proj)
-        self.program["view"].write(self.camera.view)
+        self.program["model"].write(self.camera.model)  # type: ignore
+        self.program["proj"].write(self.camera.proj)  # type: ignore
+        self.program["view"].write(self.camera.view)  # type: ignore
 
         self.vao.render()
         pygame.display.flip()
 
     def run(self) -> None:
-        self.running = True
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            self.camera.update()
             self.render()
+
+            _ = self.clock.tick(144)
 
         pygame.quit()
 
