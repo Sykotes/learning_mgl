2611010976632e0b8d6d571b5bf62feeeeae82db
Sykotes <sykotes@icloud.com>
2025-01-30 11:48:20 +0000

    setup for frag shaders

--- whatisafragmentshader.py
+++ whatisafragmentshader.py
@@ -14,6 +14,9 @@ _ = pygame.display.set_mode((WIDTH, HEIGHT), pygame.OPENGL | pygame.DOUBLEBUF)
 ctx = mgl.create_context()
 ctx.gc_mode = "auto"
 
+with open("frags/uniforms.frag") as file:
+    fragment_shader = file.read()
+
 program = ctx.program(
     vertex_shader="""
         # version 330 core
@@ -27,18 +30,7 @@ program = ctx.program(
             gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
         }
     """,
-    fragment_shader="""
-        # version 330 core
-
-        uniform vec2 u_resolution;
-        uniform vec2 u_mouse;
-        uniform float u_time;
-
-        void main() {
-            vec2 st = gl_FragCoord.xy / u_resolution;
-            gl_FragColor = vec4(st.x,st.y,0.0,1.0);
-        }
-    """,
+    fragment_shader=fragment_shader,
 )
 
 vao = ctx.vertex_array(program, [])
@@ -55,10 +47,13 @@ while running:
     width, height = pygame.display.get_window_size()
     program["u_resolution"] = glm.vec2(width, height)
 
-    # mouse_x, mouse_y = pygame.mouse.get_pos()
-    # program["u_mouse"] = glm.vec2(mouse_x, mouse_y)
+    mouse_pos = mouse_y = pygame.mouse.get_pos()
+    mouse_x = mouse_pos[0]
+    mouse_y = height - mouse_pos[1]
 
-    # program["u_time"] = time.time() - start_time
+    program["u_mouse"] = glm.vec2(mouse_x, mouse_y)
+
+    program["u_time"] = time.time() - start_time
 
     ctx.clear(0.0, 0.0, 0.0, 1.0)
     vao.render(mgl.TRIANGLES, vertices=3)
